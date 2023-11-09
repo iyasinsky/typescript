0d88073e5d9d87153a9b598b3ece3f118b0a032a
Igor Yasinsky <iyasinskyy@gmail.com>
2023-11-09 14:50:43 +0200

    Completed the 3rd and 4th tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,41 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number = Math.random()) {}
+  getSignature = (): number => this.signature;
+}
 
+class Person {
+  constructor(private key: Key) {}
+  getKey = (): Key => this.key;
+}
+
+abstract class House {
+  protected door = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected houseKey: Key) {}
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person enters the house');
+    }
+  }
+
+  abstract openDoor(doorKey: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(myDoorKey: Key): void {
+    if (myDoorKey.getSignature() === this.houseKey.getSignature()) {
+      this.door = true;
+      console.log('The door of MyHouse is open');
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
